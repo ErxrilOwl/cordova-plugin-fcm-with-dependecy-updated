ba463dd294245a7b4135196c274067752a483d5c
Nikko <atuan.nikko@gmail.com>
2023-02-14 10:44:00 +0800

    update: dependency bat

--- scripts/install_ionic_dependencies.bat
+++ scripts/install_ionic_dependencies.bat
@@ -1,6 +1,9 @@
 cd node_modules
 cd cordova-plugin-fcm-with-dependecy-updated
-cd scripts
-node install_ionic_dependencies.js ionic --legacy-peer-deps
-node install_ionic_dependencies.js ionic/ngx --legacy-peer-deps
-node install_ionic_dependencies.js ionic/v4 --legacy-peer-deps
+cd ionic
+npm i --loglevel error --no-progress
+cd ngx
+npm i --loglevel error --no-progress
+cd ..
+cd v4
+npm i --loglevel error --no-progress
